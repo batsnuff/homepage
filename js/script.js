5f66834685755baddcda7789455628e0ae81ac32
Milosz Szczepaniak <miloszszczepaniak@gmail.com>
2023-04-26 14:26:08 +0200

    function added

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
-console.log("Hello World !");
-
-let button = document.querySelector(".js-button");
-let body = document.querySelector(".body");
-let themeName = document.querySelector(".themeName");
-
-button.addEventListener("click", () => {
-  body.classList.toggle("dark");
-  if (body.classList.contains("dark")) {
-    themeName.innerText = "jasny";
-  } else {
-    themeName.innerText = "ciemny";
+{
+  const welcome = () => {
+  console.log("Witam wszystkich, którzy tutaj zaglądają 😁");
   }
-});
 
-(function () {
-  let cpr = document.getElementById("copyright");
-  cpr.innerHTML =
-    "&copy; 2022 - " +
-    new Date().getFullYear() +
-    " BATSNUFF - All Rights Reserved.";
-})();
+  welcome()  
+
+  let button = document.querySelector(".js-button");
+  let body = document.querySelector(".body");
+  let themeName = document.querySelector(".themeName");
+
+  button.addEventListener("click", () => {
+    body.classList.toggle("dark");
+    if (body.classList.contains("dark")) {
+      themeName.innerText = "jasny";
+    } else {
+      themeName.innerText = "ciemny";
+    }
+  });
+
+  (function () {
+    let cpr = document.getElementById("copyright");
+    cpr.innerHTML =
+      "&copy; 2022 - " +
+      new Date().getFullYear() +
+      " BATSNUFF - All Rights Reserved.";
+  })();
+}
